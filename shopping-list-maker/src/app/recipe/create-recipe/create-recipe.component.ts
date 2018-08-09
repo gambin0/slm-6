83c28ae41d0529a2fdb585ce30381da49779599b
gambin0 <charlesung@gmail.com>
2018-08-08 21:44:45 -0500

    wip

--- shopping-list-maker/src/app/recipe/create-recipe/create-recipe.component.ts
+++ shopping-list-maker/src/app/recipe/create-recipe/create-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-recipe',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRecipeComponent implements OnInit {
 
-  constructor() { }
+  recipeForm = this.fb.group({
+      name: ['', Validators.required],
+      link: [''],
+      servings: [0, Validators.required],
+      duration: [0, Validators.required ],
+      items: this.fb.array([
+        this.buildItem()
+      ])
+
+    });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  buildItem(): FormGroup {
+    return this.fb.group({
+      item: this.fb.group({
+        name: [],
+        
+      }),
+      measurement: this.fb.group({
+        type: [],
+        value: []
+      })
+    });
   }
 
 }
